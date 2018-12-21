5eebefffdc0a1ac41750cb666f404dc22c1503c2
Aaron Giesbrecht <aaronr.giesbrecht@gmail.com>
2018-12-20 22:21:44 -0800

    typo with cli

--- gear_compare.py
+++ gear_compare.py
@@ -2,7 +2,7 @@ from app import create_app, db, cli
 from app.models import Game_info, User
 
 app = create_app()
-Cli.register(app)
+cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
